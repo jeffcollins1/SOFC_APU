5b02a61cfadf5c14f236a5d218c8b51eae790a87
Dustin McLarty <dustin.mclarty@wsu.edu>
2018-10-05 10:07:36 -0700

    more generalization
    pulled some weight factors out to test_cycle.
    have functions running after scalinf, so that saved parameters reflect nominal sized operating conditions
    pulled out flight power and efficiency from flight_profile
    made it so that thrust profile calculations are used for flight_profile

--- Dustin_edits/system_weight.m
+++ Dustin_edits/system_weight.m
@@ -1,9 +1,9 @@
 function [weight,options] = system_weight(options,scale,FC,OTM,HL,A1)
 options.SOFC_area = scale.*options.SOFC_area;
-weight.sofc = options.SOFC_area.*10000/FC.cell_area.*0.05508; %Weight per cell, kg
+weight.sofc = options.SOFC_area.*options.sofc_specific_mass;
 
 options.OTM_area = scale.*options.OTM_area;
-weight.otm = options.OTM_area.*0.048907.*10000./81; %Weight per cell of each 81 cm^2 OTM membrane, kg
+weight.otm = options.OTM_area.*options.OTM_specific_mass;
 
 mass_flow = 28.84*scale;
 PratioComp = sqrt(options.P_non_perm./A1.P); %Pressure ratio required for a two stage turbine and two stage compressor
@@ -16,10 +16,8 @@ PratioTurb = sqrt(options.P_non_perm./A1.P);
 turbine_mass = -0.381*PratioTurb.^2 + 5.5*PratioTurb + 1.9167; %Relation for Turbine mass as a function of pressure ratio, 
 weight.turb = 2*turbine_mass.*mass_flow./1.16; 
 
-Pden_hx = 15; %Power density of heat exchangers, kW/kg
-weight.hx = scale.*(OTM.Q_out + OTM.heat_added + HL.Qremove_fuel + HL.Q_preheat + HL.Q_removed + OTM.Q_oxygen_HX)/Pden_hx; %Heat exchanger weight
+weight.hx = scale.*(OTM.Q_out + OTM.heat_added + HL.Qremove_fuel + HL.Q_preheat + HL.Q_removed + OTM.Q_oxygen_HX)/options.heat_exchange_power_den; %Heat exchanger weight
 
-Pden_motor = 24; %Power density of HTSM
-weight.motor = options.motor_eff.*(FC.Power + OTM.net_work + HL.blower_work)./Pden_motor; %Weight of propulstion motors
-weight.propulsor = 0.3*4*(9670/2.2); %Weight propulsor portion of 4 RR RB-211 engines
+weight.motor = options.motor_eff.*(FC.Power + OTM.net_work + HL.blower_work)./options.motor_power_den; %Weight of propulstion motors
+weight.propulsor = options.propulsor_weight; 
 end
--- Dustin_edits/test_cycle.m
+++ Dustin_edits/test_cycle.m
@@ -5,7 +5,7 @@ n2 = 10; % number of points in test dimension 2
 options.OTM_area = 2e3*ones(n1,n2); %membrane area in m^2 per kmol airflow
 options.SOFC_area = linspace(1e3,5e3,n1)'*ones(1,n2); %membrane area in m^2 per kmol airflow
 options.dT_fc = 50*ones(n1,n2); %Maximum temperature differential, Kelvin
-options.asr = 0.15*ones(n1,n2); % Area specific resistance, ohm-cm^2
+options.asr = 0.3*ones(n1,n2); % Area specific resistance, ohm-cm^2
 options.P_fc = 1000*ones(n1,n2); %Operating pressure for SOFC
 options.T_fc = 1023*ones(n1,n2); %Inlet temperature for SOFC
 options.T_otm = options.T_fc; %Operating temperature for OTM
@@ -20,19 +20,25 @@ options.P_non_perm = 50*ones(n1,n2); %Range of intake pressures for OTM, kPa
 options.C1_eff = 0.80*ones(n1,n2); %Mechanical efficiency of compressor 1
 options.airflow = ones(n1,n2); %Initial airflow, kmol/s
 options.P_perm = 50*ones(n1,n2); %Pressure of OTM oxygen stream, kPa; 
-options.T1_eff = 0.9*0.88*ones(n1,n2); %Mechanical efficiency of turbine
-options.C2_eff = 0.9*0.80*ones(n1,n2); %Mechanical efficiency of compressor 2
+options.T1_eff = 0.88*ones(n1,n2); %Mechanical efficiency of turbine
+options.C2_eff = 0.80*ones(n1,n2); %Mechanical efficiency of compressor 2
 options.Blower_eff = 0.5*ones(n1,n2); %efficiency of blower
 options.Blower_dP = 20*ones(n1,n2); %Pressure rise in blower in kPa
 options.TO_weight = 871200/2.2*ones(n1,n2);%take-off mass in kg
 options.Lift_2_Drag = 13.5*ones(n1,n2);%lift to drag ratio
 options.prop_eff = 0.95*ones(n1,n2);%propulsor efficiency
 options.motor_eff = 0.984*ones(n1,n2);%motor efficiency
-options.air_frame_weight = (871200 - 222200 - 248072 - 0.4*9670)/2.2*ones(n1,n2);%airframe mass in kg: Max Fuel = 101000 kg.  Max payload = 112760 kg.  4 engines, each 9670lb
-[TO,Climb,Cruise] = craft(options);
+options.air_frame_weight = options.TO_weight - 101000 - 112760 - 4*9670/2.2;%airframe mass in kg: Max Fuel = 101000 kg.  Max payload = 112760 kg.  4 engines, each 9670lb
+options.propulsor_weight = 0.3*4*(9670/2.2)*ones(n1,n2); %Weight propulsor portion (30%) of 4 RR RB-211 engines
+options.motor_power_den = 24*ones(n1,n2); %Power density of HTSM
+options.OTM_specific_mass = 0.048907*10000/81*ones(n1,n2); %Weight per m^2 OTM membrane, kg:  assumes 0.048907kg/ 81cm^2 cell
+options.sofc_specific_mass = 0.05508*10000/81*ones(n1,n2); %Weight per m^2, kg:  assumes 0.05508kg/ 81cm^2 cell
+%% should edit to be fcn of minimum temperature difference
+options.heat_exchange_power_den = 15*ones(n1,n2); %Power density of heat exchangers, kW/kg
+
 band = [0;500;4500;9500;19500;29500;39500;]/3.1;%altitude converted to m
 mission.alt = [(band(2:end)+band(1:end-1))/2;band(end)];
-mission.duration = [(band(2:end)+band(1:end-1))/3.54/3600;11];%assume a steady climb rate of 3.54m/s = climb to 39500 ft in 1 hr, then 11 hours cruise
+mission.duration = [(band(2:end)-band(1:end-1))/7.5/3600;14.38];%assume a steady climb rate of 7.5m/s , then 14.38 hours cruise
 mission.mach_num = min(0.8,0.5+0.3*mission.alt/9000);
 tic
 param = run_cycle(options,mission);
